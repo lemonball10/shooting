539f7fd89bd9f412a39be2418c649687bca03036
lemonball10 <ryj-410-fjd@e-net.nara.jp>
2025-10-31 06:20:09 +0000

    衝突判定の追加

--- game/js/main.js
+++ game/js/main.js
@@ -1,70 +1,74 @@
-import { drawPlayer, initPlayer, player } from "./player.js";
-import { spawbEnemy , enemies } from "./enemines.js";
+
+import { player, initPlayer, drawPlayer } from "./player.js";
+import { spawnEnemy, enemies, updateEnemies, drawEnemies} from "./enemies.js";
+import { handleCollisions } from "./colllision.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 initPlayer(canvas);
-spawbEnemy(canvas);
 
-const bullets = [];
-const BULLET_SPEED = -10;
+export const bullets = [];
+const BULLET_SPEED = -20;
 
-function tryShoot(){
-  bullets.push({
-    x: player.x,
-    y: player.y,
-    width: 5,
-    height: 5,
-    vy: BULLET_SPEED,
-  })
+function tryShoot() {
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        width: 10,
+        height: 10,
+        vy: BULLET_SPEED,
+    })
 }
-window.addEventListener("keydown",(e) => {
-    if(e.key ==="ArrowLeft"){
-      if(player.x > 10){
-        player.x -= 10;
-      }
+
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        if (player.x > 10) {
+            player.x -= 10;
+        }
     } else if (e.key === "ArrowRight") {
-      if(player.x < canvas.width - player.width -10){
-        player.x += 10;
-      }
-    }else if (e.code === "Space") {
-     tryShoot();
+        if (player.x < canvas.width - player.width - 10) {
+            player.x += 10;
+        }
+    } else if (e.code === "Space") {
+        tryShoot();
     }
 });
-function update(){
-  for (let i = 0; i < bullets.length; i++) {
-    const bullet = bullets[i];
-    bullet.y += bullet.vy;
-    if(bullet.y < 0) {
-      bullets.splice(i,1);
-    }
-  }
-}
 
-let y1 = 0;
-let y2 = -150;
+function update() {
+    for (let i = 0; i < bullets.length; i++) {
+        const bullet = bullets[i];
+        bullet.y += bullet.vy;
+        if (bullet.y < 0) {
+            bullets.splice(i, 1);
+        }
+    }
+    spawnEnemy(canvas);
+    updateEnemies(canvas);
+    handleCollisions();
+}
 
 function draw() {
-  ctx.fillStyle="black";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-  
-  drawPlayer(ctx);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "white" ;
-  for (let i = bullets.length - 1; i >= 0 ; i--) {
-    const bullet = bullets[i];
-    ctx.fillRect(bullet.x, bullet.y ,bullet.width, bullet.height);
-  }
+    drawPlayer(ctx);
 
-    ctx.fillStyle = "red" ;
-  for (let i = 0.length - 1; i >= 0 ; i++) {
-    const enemy = enemies[i];
-    ctx.fillRect(bullet.x, bullet.y ,bullet.width, bullet.height);
+    ctx.fillStyle = "white";
+    for (let i = 0; i < bullets.length; i++) {
+        const bullet = bullets[i];
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    }
+
+    drawEnemies(ctx);
 }
-  function gameLoop(){
+
+function gameLoop() {
     update();
     draw();
-  requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
+
--- game/js/player.js
+++ game/js/player.js
@@ -1,10 +1,10 @@
 export const player = {
-  x: 0,
-  y: 0,
-  width: 30,
-  height: 30,
-  color: "yellow",
-  life: 3,
+    x: 0,
+    y: 0,
+    width: 30,
+    height: 30,
+    color: "yellow",
+    life: 3,
 };
 
 export function initPlayer(canvas) {
@@ -12,7 +12,8 @@ export function initPlayer(canvas) {
     player.y = canvas.height - 60;
     console.log("Player:", player);
 }
+
 export function drawPlayer(ctx) {
-  ctx.fillStyle =player.color;
-  ctx.fillRect(player.x,player.y,player.width,player.height);
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
 }
